650ea7585a97eeb3b22316b921f3806946a6b2ed
Robert Gerus <ar@is-a.cat>
2023-06-06 18:28:14 +0200

    meow: publish button is now an animated pride rainbow
    based on the change sdomi made for her (akkoma) instance

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -305,6 +305,7 @@ class ComposeForm extends ImmutablePureComponent {
                 <Button
                   type='submit'
                   compact
+                  className='pride-gradient'
                   text={intl.formatMessage(this.props.isEditing ? messages.saveChanges : (this.props.isInReply ? messages.reply : messages.publish))}
                   disabled={!this.canSubmit()}
                 />
